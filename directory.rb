d99616d93e60460b11d81341aaefd61a81991d60
Callum <Callum@hotmail.com>
2023-02-10 01:56:41 +0000

    added save feature

--- directory.rb
+++ directory.rb
@@ -1,34 +1,84 @@
-# let's pull all students into an array
-students = [
+@students = []
 
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "9. Exit" 
+end
+
+def interactive_menu
+    loop do
+      print_menu
+      process(gets.chomp)
+    end
+  end
+
+def process(selection)
+    case selection
+      when "1"
+        input_students
+      when "2"
+        show_students
+      when "3"
+        save_students
+      when "9"
+        exit
+      else
+        puts "I don't know what you mean, try again"
+    end
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+
+  students = []
+
+  name = gets.chomp
+
+  until name.empty?
+
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp
+  end
+
+  students
+end
+
+def save_students
+
+    file = File.open("students.csv", "w")
+
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
+end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
 
-def print(students)
-    students.each { |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    }
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@9students.count} great students"
 end
 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
